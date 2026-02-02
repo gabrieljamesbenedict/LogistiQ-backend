1de06cf1b654804724f9cb0e5621e780f39622ad
Gabriel James Benedict <benok.loslos@gmail.com>
2026-02-03 05:54:03 +0800

    Initial Commit

---
+++ src/main/java/com/porado/LogistiQ/backend/LogistiQApplication.java
@@ -0,0 +1,13 @@
+package com.porado.LogistiQ.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogistiQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogistiQApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porado/LogistiQ/backend/LogistiQApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porado.LogistiQ.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogistiQApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
