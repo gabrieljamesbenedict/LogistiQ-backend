c1783091c5b0c19086f8e914f2eefaf797960b10
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-13 08:36:02 +0800

    refactor: redo all service implementation classes

--- src/main/java/com/porado/LogistiQ/backend/serviceImpl/EmployerServiceImpl.java
+++ src/main/java/com/porado/LogistiQ/backend/serviceImpl/EmployerServiceImpl.java
@@ -3,27 +3,46 @@ package com.porado.LogistiQ.backend.serviceImpl;
 import com.porado.LogistiQ.backend.model.Employee;
 import com.porado.LogistiQ.backend.service.EmployerService;
 import com.porado.LogistiQ.backend.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class EmployerServiceImpl implements EmployerService {
 
     private final EmployeeRepository employeeRepository;
 
-    @Autowired
-    public EmployerServiceImpl(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
+    @Override
+    public Employee getEmployee(Long id){
+        return employeeRepository
+                .findById(id)
+                .orElseThrow(() -> new IllegalStateException("Employee does not exist"));
     }
 
     @Override
-    public Employee addDriver(Employee employee) {
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee createEmployee(Employee employee) {
+        if (employee.getEmployeeId() != null) throw new IllegalStateException("Cannot create a new Employee record");
         return employeeRepository.save(employee);
     }
 
     @Override
-    public List<Employee> getAllDrivers() {
-        return employeeRepository.findAll();
+    public Employee updateEmployee(Employee employee) {
+        if (employee.getEmployeeId() == null) throw new IllegalStateException("Cannot update on null ID");
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public void deleteEmployee(Employee employee) {
+        employeeRepository.delete(employee);
     }
 }
--- src/main/java/com/porado/LogistiQ/backend/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/porado/LogistiQ/backend/serviceImpl/UserServiceImpl.java
@@ -1,30 +1,66 @@
 package com.porado.LogistiQ.backend.serviceImpl;
 
+import com.porado.LogistiQ.backend.model.Employee;
 import com.porado.LogistiQ.backend.model.User;
 import com.porado.LogistiQ.backend.repository.UserRepository;
 import com.porado.LogistiQ.backend.service.UserService;
+import com.porado.LogistiQ.backend.util.UserRole;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
     @Override
-    public User registerUser(String username, String rawPassword, String role) {
+    public User registerUser(String username, String password) {
         if (userRepository.existsByUsername(username)) {
             throw new IllegalArgumentException("Username already exists");
         }
-        String encodedPassword = passwordEncoder.encode(rawPassword);
-        User user = new User(username, encodedPassword, role);
+        String encodedPassword = passwordEncoder.encode(password);
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setRole(UserRole.ROLE_EMPLOYEE);
         return userRepository.save(user);
+    }
+
+    @Override
+    public User getUser(Long id) {
+        return userRepository
+                .findById(id)
+                .orElseThrow(() -> new IllegalStateException("Employee does not exist"));
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User createUser(User user) {
+        if (user.getUserId() != null) throw new IllegalStateException("Cannot create a new User record");
+        userRepository.save(user);
+        return null;
+    }
+
+    @Override
+    public User updateUser(User user) {
+        if (user.getUserId() == null) throw new IllegalStateException("Cannot update on null ID");
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(User user) {
+        userRepository.delete(user);
     }
 }
 
