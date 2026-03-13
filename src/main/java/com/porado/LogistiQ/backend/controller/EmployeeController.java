4569dd338c996f56cd121b3184616450a35fd167
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-13 10:04:03 +0800

    refactor: rename mapping from /api/drivers to /api/employees for EmployeeController

--- src/main/java/com/porado/LogistiQ/backend/controller/EmployeeController.java
+++ src/main/java/com/porado/LogistiQ/backend/controller/EmployeeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/drivers")
+@RequestMapping("/api/employees")
 @RequiredArgsConstructor
 public class EmployeeController {
 
