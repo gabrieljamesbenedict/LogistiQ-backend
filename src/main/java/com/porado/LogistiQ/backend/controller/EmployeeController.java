9c1e3967fd16c972002337152fdb94fcd5112254
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-13 08:59:40 +0800

    fix: create RESTful API methods for EmployeeController

--- src/main/java/com/porado/LogistiQ/backend/controller/EmployeeController.java
+++ src/main/java/com/porado/LogistiQ/backend/controller/EmployeeController.java
@@ -2,29 +2,51 @@ package com.porado.LogistiQ.backend.controller;
 
 import com.porado.LogistiQ.backend.model.Employee;
 import com.porado.LogistiQ.backend.service.EmployerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/drivers")
-@CrossOrigin(origins = "http://localhost:3000")
+@RequiredArgsConstructor
 public class EmployeeController {
 
     private final EmployerService employerService;
 
-    @Autowired
-    public EmployeeController(EmployerService employerService) {
-        this.employerService = employerService;
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployee(@PathVariable Long id) {
+        return ResponseEntity.ok(employerService.getEmployee(id));
     }
 
-    @PostMapping("/add")
-    public Employee addDriver(@RequestBody Employee employee) {
-        return employerService.addDriver(employee);
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return ResponseEntity.ok(employerService.getAllEmployees());
     }
 
-    @GetMapping("/all")
-    public List<Employee> getAllDrivers() {
-        return employerService.getAllDrivers();
+    @PostMapping
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+        return ResponseEntity.ok(employerService.createEmployee(employee));
     }
+
+    @PutMapping
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+        return ResponseEntity.ok(employerService.updateEmployee(employee));
+    }
+
+    @DeleteMapping
+    public void deleteEmployee(@RequestBody Employee employee) {
+        employerService.deleteEmployee(employee);
+    }
+
+//    @PostMapping("/add")
+//    public Employee addDriver(@RequestBody Employee employee) {
+//        return employerService.addDriver(employee);
+//    }
+//
+//    @GetMapping("/all")
+//    public List<Employee> getAllDrivers() {
+//        return employerService.getAllDrivers();
+//    }
 }
